f9fd113173e3342be3cd13340fafba0e27af9bb2
ShadowATRX <141434243+atrx07@users.noreply.github.com>
2026-06-02 10:56:52 +0530

    Update chatroom.js

--- worker/src/chatroom.js
+++ worker/src/chatroom.js
@@ -24,7 +24,7 @@ export class ChatRoom {
       const displayName = url.searchParams.get('name') || 'user';
 
       const [client, server] = Object.values(new WebSocketPair());
-      server.accept();
+      this.state.acceptWebSocket(server, [uid]);
 
       const session = { uid, displayName };
       this.sessions.set(server, session);
